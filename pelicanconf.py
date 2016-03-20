fd51a0654aa5d2fba8821bd4ad4fe7bce93a392b
Martin Brochhaus <mbrochh@gmail.com>
2016-03-20 11:54:50 +0800

    Updated about-tagline

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ THEME = '/Users/martin/Projects/pelican-responsive/src/responsive'
 AUTHOR = u'Martin Brochhaus'
 SITENAME = u'martinbrochhaus.com'
 SITEURL = 'http://martinbrochhaus.com'
-MINI_BIO = u"I build software with Python & Django. Every day. All day long."
+MINI_BIO = u"I build software with Python, Django, ReactJS & React-Native. Every day. All day long."
 BIO = u'<strong>Martin Brochhaus</strong> is the founder of <a href="http://bitmazk.com">Bitmazk Pte. Ltd.</a>. He is doing web development since 1998 and currently he is in love with Python & Django. Almost all of his work can be found on <a href="https://github.com/bitmazk">Github</a>. When he is not writing code, he gives <a href="https://speakerdeck.com/mbrochh">talks</a> or helps organizing <a href="https://pycon.sg">conferences</a>.'
 
 TIMEZONE = 'Asia/Singapore'
